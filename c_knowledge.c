83878d333a7f59f5e9c2a2e8d8bc8f28ab5b708d
York Wu <qiangpipi@msn.com>
2015-12-15 12:46:10 +0800

    Create c_knowledge.c

---
+++ c_knowledge.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define min_t(type,x,y) \
+        ({type __x=(x); type __y=(y); __x<__y?__x:__y;})
+#define min(x,y) \
+        ({const typeof(x) __x=x;const typeof(y) __y=y;(void)(&__x==&__y);__x<__y?__x:__y;})
+
+int main(){
+        int ia,ib,mini;
+        float fa,fb,minf;
+        ia=1;ib=2;
+        fa=1.0;fb=2.0;
+//Use define min_t
+        mini=min_t(int,++ia,++ib);
+        minf=min_t(float,fa,fb);
+        printf("min_t: min int: %d\n",mini);
+        printf("min_t: min float: %f\n",minf);
+//Use define min
+        mini=min(ia,ib);
+        minf=min(fa,fb);
+        printf("min: min int: %d\n",mini);
+        printf("min: min float: %f\n",minf);
+        return 0;
+}
