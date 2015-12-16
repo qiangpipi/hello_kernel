4306cf7957ed3cef13f492d80494a1e4a7d3766d
kkk <kkk@kkk.oowvps.com>
2015-12-16 09:08:13 +0800

    Add more c knowledge file

---
+++ knowledge/3.3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define min_t(type,x,y) \
+	({type __x=(x); type __y=(y); __x<__y?__x:__y;})
+#define min(x,y) \
+	({const typeof(x) __x=x;const typeof(y) __y=y;(void)(&__x==&__y);__x<__y?__x:__y;})
+#define pr_debug(fmt,arg...) \
+	printf(fmt,##arg)
+
+int main(){
+	int ia,ib,mini;
+	float fa,fb,minf;
+	ia=1;ib=2;
+	fa=1.0;fb=2.0;
+//Use define min_t
+	mini=min_t(int,++ia,++ib);
+	minf=min_t(float,fa,fb);
+	printf("min_t: min int: %d\n",mini);
+	printf("min_t: min float: %f\n",minf);
+//Use define min
+	mini=min(ia,ib);
+	minf=min(fa,fb);
+	printf("min: min int: %d\n",mini);
+	printf("min: min float: %f\n",minf);
+	pr_debug("Test print 1 without arg\n");
+	pr_debug("Test print with ia: %d and fa: %f\n",ia,fa);
+	pr_debug("File: %s, Function: %s, Line: %d\n",__FILE__,__FUNCTION__,__LINE__);
+	pr_debug("File: %s, Function: %s, Line: %d\n",__FILE__,__PRETTY_FUNCTION__,__LINE__);
+	return 0;
+}
